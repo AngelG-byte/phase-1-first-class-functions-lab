2dda0745d137465341801668ad13e26e17a37909
Angel Marte <robotking134@gmail.com>
2022-10-12 17:03:20 -0400

    commit tree 034e40b0c46b0afe22996f93f3fca3827c342601

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0,2)
+}
+const returnLastTwoDrivers = function(){
+    return drivers.slice(2,4)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier (num1){
+    return function  (num2){
+        return num1 * num2
+    }
+}
+   
+  function fareDoubler(num){
+    return num * 2
+  }
+  function fareTripler(a){
+    return a * 3
+  }
+  function selectDifferentDrivers(drivers,a){
+    return a(drivers)
+}
+
+
